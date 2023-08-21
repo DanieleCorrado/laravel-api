7d1ce56b0273b620c52fa056cd84ec313246ee2c
DanieleCorrado <corrado_daniele@virgilio.it>
2023-08-21 16:25:58 +0200

    add api ProjectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function projectIndex() {
+
+        $projects = Project :: with('type') -> paginate(10);
+
+        return response() -> json([
+            'projects' => $projects
+        ]);
+    }
+}
